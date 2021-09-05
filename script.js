0d4a1ae3397b3779f9b626d29a6ab8aea8e6700d
Jithin U S <jithinustech1@gmail.com>
2021-09-05 20:38:57 +0530

    files modified

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function displayCountry(country) {
     let html_data = `<div class="card">
     <img src="${flag}" class="img" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${country_name}</h5>
+      <h5 class="card-title" style="color: whitesmoke;">${country_name}</h5>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">Population : ${population}</li>
